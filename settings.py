31bc9bedeb85f63fa6e3029c57d7f43d69df463f
i074455 <xiaobing.huang@sap.com>
2020-10-09 20:45:02 +0800

    squash! fixes #3: Added Python3 compatibility

--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
 SLEEP_BETWEEN_PAGES = 4000
 
 EXCLUDED_DOMAINS = (
-    'ajax.googleapis.com', 
-    'www.google-analytics.com', 
-    'code.angularjs.org', 
-    'code.jquery.com',
+    "ajax.googleapis.com",
+    "www.google-analytics.com",
+    "code.angularjs.org",
+    "code.jquery.com",
+    "test.change.com",
 )
